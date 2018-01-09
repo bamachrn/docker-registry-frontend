381b18d6cbfcd46a9003997e412d97dece5d733a
Bamacharan Kundu <bamachrn@gmail.com>
2018-01-09 20:10:37 +0530

    Moved the home page to about page and updated the default route

--- app/app.js
+++ app/app.js
@@ -39,23 +39,19 @@ angular
     'ui.checkbox'
   ])
   .config(['$routeProvider', '$resourceProvider', 'cfpLoadingBarProvider', '$locationProvider',
-      function($routeProvider, $resourceProvider, cfpLoadingBarProvider, $locationProvider){
+    function($routeProvider, $resourceProvider, cfpLoadingBarProvider, $locationProvider) {
 
-     $locationProvider.html5Mode(true);
+      $locationProvider.html5Mode(true);
 
-    // Don't show the spinner when making XHR requests.
-    // Also, show the bar only if an XHR request takes longer than 50ms.
-    cfpLoadingBarProvider.includeSpinner = false;
-    cfpLoadingBarProvider.latencyThreshold = 10;
+      // Don't show the spinner when making XHR requests.
+      // Also, show the bar only if an XHR request takes longer than 50ms.
+      cfpLoadingBarProvider.includeSpinner = false;
+      cfpLoadingBarProvider.latencyThreshold = 10;
 
-    // Don't strip trailing slashes from calculated URLs
-    $resourceProvider.defaults.stripTrailingSlashes = false;
+      // Don't strip trailing slashes from calculated URLs
+      $resourceProvider.defaults.stripTrailingSlashes = false;
 
-    $routeProvider.
-      when('/home', {
-        templateUrl: 'home.html',
-        controller: 'HomeController',
-      }).
+      $routeProvider.
       when('/repositories/:reposPerPage?/:lastNamespace?/:lastRepository?', {
         templateUrl: 'repository/repository-list.html',
         controller: 'RepositoryListController'
@@ -68,7 +64,7 @@ angular
         templateUrl: 'repository/repository-detail.html',
         controller: 'RepositoryDetailController'
       }).
-	    when('/about', {
+      when('/about', {
         templateUrl: 'about.html',
       }).
       when('/tag/:repositoryUser?/:repositoryName/:tagName/', {
@@ -84,6 +80,7 @@ angular
         controller: 'CreateTagController',
       }).
       otherwise({
-        redirectTo: '/home'
+        redirectTo: '/repository'
       });
-  }]);
+    }
+  ]);
